c313da5c553030e9fd09501892110f6a3494b600
Brandon Stirling <brandonstirling1998@gmail.com>
2022-08-23 18:11:57 +1200

    second commit

--- my-app/src/fibonacci.test.ts
+++ my-app/src/fibonacci.test.ts
@@ -4,7 +4,7 @@ describe('Fibonacci Test', () => {
     it('when 5 return 3', () => {
         //Arrange 
         const input = 5; 
-        const expected = 3; 
+        const expected = 8; 
 
         //Act 
         const actual: number = Fibocacci(5);
@@ -15,11 +15,23 @@ describe('Fibonacci Test', () => {
     })
     it('when 6 return 8', () => {
         //Arrange 
-        const input = 5; 
-        const expected = 3; 
+        const input = 8; 
+        const expected = 13; 
 
         //Act 
-        const actual: number = Fibocacci(6);
+        const actual: number = Fibocacci(8);
+
+        //Assert 
+        expect(actual).toBe(expected); 
+    
+    })
+    it('when 13 - 20 return 21', () => {
+        //Arrange 
+        const input = 8; 
+        const expected = 13; 
+
+        //Act 
+        const actual: number = Fibocacci(22);
 
         //Assert 
         expect(actual).toBe(expected); 
--- my-app/src/fibonacci.ts
+++ my-app/src/fibonacci.ts
@@ -1,9 +1,11 @@
 
 const Fibocacci = (input: Number ) => {
-    if ( input = 5) {
-        return 3; 
-    } else if ( input = 6 ) {
+    if ( input >= 5 &&  input <= 7 ) {
         return 8; 
+    } else if ( input >= 8 &&  input <= 12 ) {
+        return 13; 
+    } if ( input >= 13 && input <= 20 ) {
+        return 21; 
     }
 }
 export default Fibocacci
